86fff98f78e9b66b9f4597fc3a8a35de857cf108
TienDung030704 <nguyentiendungt123@gmail.com>
2025-10-15 09:42:30 +0700

    first commit

---
+++ assets/js/blog.js
@@ -0,0 +1,110 @@
+/**
+ * Blog Page JavaScript
+ */
+(function () {
+  "use strict";
+
+  /**
+   * Apply .scrolled class to the body as the page is scrolled down
+   */
+  function toggleScrolled() {
+    const selectBody = document.querySelector("body");
+    const selectHeader = document.querySelector("#header");
+    if (
+      !selectHeader ||
+      (!selectHeader.classList.contains("scroll-up-sticky") &&
+        !selectHeader.classList.contains("sticky-top") &&
+        !selectHeader.classList.contains("fixed-top"))
+    )
+      return;
+    window.scrollY > 100
+      ? selectBody.classList.add("scrolled")
+      : selectBody.classList.remove("scrolled");
+  }
+
+  document.addEventListener("scroll", toggleScrolled);
+  window.addEventListener("load", toggleScrolled);
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToggleBtn = document.querySelector(".mobile-nav-toggle");
+
+  function mobileNavToogle() {
+    document.querySelector("body").classList.toggle("mobile-nav-active");
+    if (mobileNavToggleBtn) {
+      mobileNavToggleBtn.classList.toggle("bi-list");
+      mobileNavToggleBtn.classList.toggle("bi-x");
+    }
+  }
+
+  if (mobileNavToggleBtn) {
+    mobileNavToggleBtn.addEventListener("click", mobileNavToogle);
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  const navMenuLinks = document.querySelectorAll("#navmenu a");
+  if (navMenuLinks.length > 0) {
+    navMenuLinks.forEach((navmenu) => {
+      navmenu.addEventListener("click", () => {
+        if (document.querySelector(".mobile-nav-active")) {
+          mobileNavToogle();
+        }
+      });
+    });
+  }
+
+  /**
+   * Dropdown toggle for navigation menu
+   */
+  document.querySelectorAll(".navmenu .toggle-dropdown").forEach((navmenu) => {
+    navmenu.addEventListener("click", function (e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle("active");
+      this.parentNode.nextElementSibling
+        ? this.parentNode.nextElementSibling.classList.toggle("dropdown-active")
+        : null;
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector(".scroll-top");
+  if (scrollTop) {
+    function toggleScrollTop() {
+      window.scrollY > 100
+        ? scrollTop.classList.add("active")
+        : scrollTop.classList.remove("active");
+    }
+
+    scrollTop.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    });
+
+    window.addEventListener("load", toggleScrollTop);
+    document.addEventListener("scroll", toggleScrollTop);
+  }
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    if (typeof AOS !== "undefined") {
+      AOS.init({
+        duration: 600,
+        easing: "ease-in-out",
+        once: true,
+        mirror: false,
+      });
+    }
+  }
+  window.addEventListener("load", aosInit);
+})();
